5770cd15caaebf8aad3291f4bc77403aba1d83af
Loïck Virot <virot.loick@gmail.com>
2016-12-01 22:37:44 +0100

    Add register page

---
+++ app/view/register.php
@@ -0,0 +1,5 @@
+<form class="" action="index.html" method="post">
+  <input type="text" name="username" placeholder="Username">
+  <input type="password" name="password" placeholder="Password">
+  <input type="submit" name="name" value="Login">
+</form>
