aba90fb036f6899000cf329cd57d1be090aac33c
Kaalah <louche.fox@gmail.com>
2016-12-02 02:33:26 +0100

    page accueil

--- app/view/default/header.php
+++ app/view/default/header.php
@@ -1,15 +1,4 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<link rel="stylesheet" href="style.css" type="text/css" />
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
-<title>Untitled Document</title>
-<!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-</head>
 
-<body>
 <div class=""><!-- div exterieur-->
 <input class="open" id="top-box" type="checkbox" hidden>
 <label class="btn" for="top-box"></label>
@@ -20,5 +9,4 @@
 </div>
 <input class="open" id="top-box" type="checkbox" hidden>
 </div>
-</body>
-</html>
+
